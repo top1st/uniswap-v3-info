ee2c0cf3db3e651b08db63206c1f3b42fa9d13aa
top1st <riguang102@gmail.com>
2021-09-24 10:49:53 +0800

    rename 2

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -118,7 +118,7 @@ export default function Home() {
     <PageWrapper>
       <ThemedBackgroundGlobal backgroundColor={activeNetwork.bgColor} />
       <AutoColumn gap="16px">
-        <TYPE.main>Uniswap Overview</TYPE.main>
+        <TYPE.main>Mintyswap Overview</TYPE.main>
         <ResponsiveRow>
           <ChartWrapper>
             <LineChart
